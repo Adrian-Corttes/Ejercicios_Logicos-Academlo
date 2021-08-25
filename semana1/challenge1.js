cceb454d0d7f575d24ea4189a1985484720d656b
Adrian Cortes <ls-adrian@hotmail.com>
2021-08-25 17:23:23 -0500

    Se resolvió el ejercicio N.1 de la semana1

--- semana1/challenge1.js
+++ semana1/challenge1.js
@@ -1,6 +1,26 @@
 const reverseString = (str) => {
-    //Implementación
+    let message = '';
+    for(let i =str.length -1; i >= 0; i--){
+        message = message + str[i]
+    }
+
+    if(message.length > 15 || message.length <= 1){
+            throw new Error('Tipo de dato o longitud no admitidos');
+        }
+
+        return message
 }
+
+// const reverseString = (str) =>{
+//     str.split('').reverse().join('');
+//     let message = str;
+
+//     if(message.length > 15 || message.length <= 1){
+//         throw new Error('Tipo de dato o longitud no admitidos');
+//     }
+
+//     return message
+// }
 
 module.exports = {
     reverseString
